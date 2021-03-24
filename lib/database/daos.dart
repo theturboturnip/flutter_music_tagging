a194e954981622579c4cf96078cd9eba895376ed
Samuel Stark <samuel.winslow.stark@gmail.com>
2021-03-24 21:20:58 +0000

    Dao stubs

---
+++ lib/database/daos.dart
@@ -0,0 +1,37 @@
+import 'package:floor/floor.dart';
+import 'package:flutter/material.dart';
+
+@dao
+class SongDao {
+
+}
+
+// Album
+@dao
+class AlbumDao {
+
+}
+
+// Artist
+@dao
+class ArtistDao {
+
+}
+
+// Playlist
+@dao
+class PlaylistDao {
+
+}
+
+// Folder
+@dao
+class FolderDao {
+
+}
+
+// Tag
+@dao
+class TagDao {
+    // TODO - functions for songs/albums/playlists/folders directly tagged, then functions for indirect tags
+}
