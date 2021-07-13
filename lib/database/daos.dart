aad90a2c285bff40d9fa526c77aafb981e54ef4e
theturboturnip <popgoestoast@gmail.com>
2021-07-13 21:35:38 +0100

    Refactored backend handling

--- lib/database/daos.dart
+++ lib/database/daos.dart
@@ -1,8 +1,11 @@
 import 'package:floor/floor.dart';
 
+import 'backend_id.dart';
 import 'models.dart';
 import 'package:flutter_music_tagging/ext.dart';
 
+// Abstract class with function annotations that will
+// automatically generate correct insert/update/delete
 abstract class BaseMutableDao<T> {
   @insert
   Future<List<T>> insertValues(List<T> values);
@@ -15,6 +18,26 @@ abstract class BaseMutableDao<T> {
   Future<void> deleteValues(List<T> values);
 }
 
+abstract class BaseMutableBackendedDao<T, TBackendElem extends BackendRow<T>> {
+  TBackendElem generateRow(T item, BackendId id);
+
+  @insert
+  Future<void> _insertBackendElems(List<TBackendElem>);
+
+  @transaction
+  Future<void> insertBackendMappings(T item, List<BackendId> items) {
+    return _insertBackendElems(items.map((e) => generateRow(item, e)));
+  }
+
+  // TODO - need update-y functions, delete-y functions
+}
+
+// Abstract class with function annotations that will
+// automatically generate correct insert/update functions for
+// "ordered-groups".
+// e.g. T = Album, TReferenced = Song, TEntry = AlbumEntry
+// will generate insertGroup(Album, List<Song>) that automatically
+// creates correct AlbumEntry rows.
 abstract class BaseOrderedGroupDao<T, TEntry, TReferenced> {
   TEntry generateEntry(T base, int index, TReferenced referenced);
   Future<void> deleteGroupEntries(T base);
@@ -42,13 +65,27 @@ abstract class BaseOrderedGroupDao<T, TEntry, TReferenced> {
 }
 
 @dao
-abstract class SongDao extends BaseMutableDao<Song> {
-  // TODO - find queries
+abstract class SongDao extends BaseMutableDao<Song> with BaseMutableBackendedDao<Song, Song_BackendRow> {
+  Song_BackendRow generateRow(Song item, BackendId id) {
+    return Song_BackendRow(item.id, id);
+  }
+
+  // TODO - more queries for selecting songs
+
+  @Query("SELECT * FROM Song where instr(title, :keyword) > 0")
+  Future<List<Song>> getSongsByKeyword(String keyword);
+
+  @Query("SELECT * FROM Song ORDER BY title")
+  Stream<List<Song>> listenForAllSongs();
 }
 
 // Album
 @dao
-abstract class AlbumDao extends BaseOrderedGroupDao<Album, AlbumEntry, Song> {
+abstract class AlbumDao extends BaseOrderedGroupDao<Album, AlbumEntry, Song> with BaseMutableBackendedDao<Album, Album_BackendRow> {
+  Album_BackendRow generateRow(Album item, BackendId id) {
+    return Album_BackendRow(item.id, id);
+  }
+  
   @Query("SELECT * FROM Album where albumId = :id")
   Future<Album> getAlbum(int id);
 
@@ -64,6 +101,9 @@ abstract class AlbumDao extends BaseOrderedGroupDao<Album, AlbumEntry, Song> {
       "ORDER BY AlbumEntries.index")
   Stream<List<Song>> listenForAlbumSongs(Album album);
 
+  @Query("SELECT * FROM Song ORDER BY title")
+  Stream<List<Song>> listenForAllAlbums();
+
   @override
   AlbumEntry generateEntry(Album base, int index, Song referenced) {
     return AlbumEntry(base.id, referenced.id, index);
@@ -76,6 +116,13 @@ abstract class AlbumDao extends BaseOrderedGroupDao<Album, AlbumEntry, Song> {
 
 // Artist
 @dao
-abstract class ArtistDao extends BaseMutableDao<Artist> {
+abstract class ArtistDao extends BaseMutableDao<Artist> with BaseMutableBackendedDao<Artist, Artist_BackendRow> {
+  Artist_BackendRow generateRow(Artist item, BackendId id) {
+    return Artist_BackendRow(item.id, id);
+  }
+
   // TODO - find queries
+
+  @Query("SELECT * FROM Song ORDER BY title")
+  Stream<List<Song>> listenForAllArtists();
 }
--- lib/database/models.dart
+++ lib/database/models.dart
@@ -1,6 +1,25 @@
 import 'package:floor/floor.dart';
 import 'package:flutter_music_tagging/database/backend_id.dart';
 
+// TODO - We need to change how backend IDs work.
+// Before, I had 1:1 Song:BackendID
+// but this has a problem when multiple services can have the same song
+// e.g. I could have a CD that's also available on Spotify.
+// Those should be treated as the same song by the app.
+// => there's a 1:Many Song:BackendID mapping, and this also holds for Albums + Artists.
+// Additionally, for each backend, there may exist multiple items -> same artist
+
+abstract class BackendRow<T> {
+  @PrimaryKey()
+  @ColumnInfo(name: "base_id")
+  final int baseId;
+
+  @ColumnInfo(name: "backend_id")
+  final BackendId backendId;
+
+  BackendRow(this.baseId, this.backendId);
+}
+
 @Entity(
   foreignKeys: [
     ForeignKey(
@@ -14,8 +33,6 @@ class Song {
   @PrimaryKey(autoGenerate: true)
   final int id;
 
-  final BackendId backendId;
-
   final String title;
   final int lengthMs;
 
@@ -23,10 +40,22 @@ class Song {
   final int? albumId;
 
   // Used to create from SQL-version
-  Song(this.id, this.backendId, this.title, this.lengthMs, this.albumId);
+  Song(this.id, this.title, this.lengthMs, this.albumId);
   // Used to create from Dart/Flutter side
-  Song.fromNew(this.backendId, this.title, this.lengthMs, this.albumId)
-      : this.id = 0;
+  Song.fromNew(this.title, this.lengthMs, this.albumId) : this.id = 0;
+}
+
+@Entity(
+  foreignKeys: [
+    ForeignKey(
+      childColumns: ['base_id'],
+      parentColumns: ['id'],
+      entity: Song,
+    )
+  ],
+)
+class Song_BackendRow extends BackendRow<Song> {
+  Song_BackendRow(int baseId, BackendId backendId) : super(baseId, backendId);
 }
 
 @Entity(
@@ -42,18 +71,28 @@ class Album {
   @PrimaryKey(autoGenerate: true)
   final int id;
 
-  final BackendId backendId;
-
   final String title;
   final int trackCount;
 
   @ColumnInfo(name: "artist_id")
   final int artistId;
 
-  Album(this.id, this.backendId, this.title, this.trackCount, this.artistId);
+  Album(this.id, this.title, this.trackCount, this.artistId);
   // Used to create from Dart/Flutter side
-  Album.fromNew(this.backendId, this.title, this.trackCount, this.artistId)
-      : this.id = 0;
+  Album.fromNew(this.title, this.trackCount, this.artistId) : this.id = 0;
+}
+
+@Entity(
+  foreignKeys: [
+    ForeignKey(
+      childColumns: ['base_id'],
+      parentColumns: ['id'],
+      entity: Album,
+    )
+  ],
+)
+class Album_BackendRow extends BackendRow<Album> {
+  Album_BackendRow(int baseId, BackendId backendId) : super(baseId, backendId);
 }
 
 @Entity(foreignKeys: [
@@ -83,18 +122,27 @@ class AlbumEntry {
   AlbumEntry(this.albumId, this.songId, this.index);
 }
 
-// Per-backend artist
-// Could be remapped later?
 @entity
 class Artist {
   @PrimaryKey(autoGenerate: true)
   final int id;
 
-  final BackendId backendId;
-
   final String name;
 
-  Artist(this.id, this.backendId, this.name);
+  Artist(this.id, this.name);
   // Used to create from Dart/Flutter side
-  Artist.fromNew(this.backendId, this.name) : this.id = 0;
+  Artist.fromNew(this.name) : this.id = 0;
+}
+
+@Entity(
+  foreignKeys: [
+    ForeignKey(
+      childColumns: ['base_id'],
+      parentColumns: ['id'],
+      entity: Album,
+    )
+  ],
+)
+class Artist_BackendRow extends BackendRow<Artist> {
+  Artist_BackendRow(int baseId, BackendId backendId) : super(baseId, backendId);
 }
