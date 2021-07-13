f661e812de5e122d09475acb9996278f4f45ade2
theturboturnip <popgoestoast@gmail.com>
2021-07-13 20:22:03 +0100

    Simplified database code for future backend setup

--- lib/database/daos.dart
+++ lib/database/daos.dart
@@ -1,5 +1,4 @@
 import 'package:floor/floor.dart';
-import 'package:sealed_unions/union_2.dart';
 
 import 'models.dart';
 import 'package:flutter_music_tagging/ext.dart';
@@ -79,46 +78,4 @@ abstract class AlbumDao extends BaseOrderedGroupDao<Album, AlbumEntry, Song> {
 @dao
 abstract class ArtistDao extends BaseMutableDao<Artist> {
   // TODO - find queries
-}
-
-// Playlist
-@dao
-abstract class PlaylistDao
-    extends BaseOrderedGroupDao<Playlist, PlaylistEntry, Song> {
-  // TODO - find queries
-
-  @override
-  PlaylistEntry generateEntry(Playlist base, int index, Song referenced) {
-    return PlaylistEntry(index, base.id, referenced.id);
-  }
-
-  @override
-  @Query("DELETE * FROM PlaylistEntry where playlist_id = :base.id")
-  Future<void> deleteGroupEntries(Playlist base);
-}
-
-// Folder
-@dao
-abstract class FolderDao
-    extends BaseOrderedGroupDao<Folder, FolderChild, Union2<Folder, Album>> {
-  // TODO - find queries
-
-  @override
-  FolderChild generateEntry(
-      Folder base, int index, Union2<Folder, Album> referenced) {
-    return referenced.join(
-        (folder) => FolderChild.folder(base.id, folder, index),
-        (album) => FolderChild.album(base.id, album, index));
-  }
-
-  @override
-  @Query("DELETE * FROM FolderChild where parent_folder_id = :base.id")
-  Future<void> deleteGroupEntries(Folder base);
-}
-
-// Tag
-@dao
-abstract class TagDao {
-  // TODO - find queries
-  // TODO - functions for songs/albums/playlists/folders directly tagged, then functions for indirect tags?
 }
